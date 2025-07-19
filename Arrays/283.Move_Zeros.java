7acf60ac298ab9b006e9ed78b5f734b999cc7bb1
Prajwal1325 <eng23ds0025@dsu.edu.in>
2025-07-19 12:00:52 +0530

    First Commit

---
+++ Arrays/283.Move_Zeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int point = 0;
+      for(int i=0;i<nums.length;i++){
+        if(nums[i] != 0){
+            nums[point] = nums[i];
+            point++;
+        }
+      }
+      while(point < nums.length){
+        nums[point] = 0;
+        point++;
+      }
+    
+    }
+  
+}
