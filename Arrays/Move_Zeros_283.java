0b640ee1986a529f5f69d57a4bf8e8fcacc68b1f
Prajwal1325 <eng23ds0025@dsu.edu.in>
2025-07-24 08:44:59 +0530

    Second commit

--- Arrays/Move_Zeros_283.java
+++ Arrays/Move_Zeros_283.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    public void Move_Zeros_283(int[] nums) {
         int point = 0;
       for(int i=0;i<nums.length;i++){
         if(nums[i] != 0){
---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args){
+        int[] nums = {3,2,4};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+
+
+    }
+
+    static int[] twoSum(int[] nums,int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i],i);
+
+        }
+        return new int[]{};
+}
+}
