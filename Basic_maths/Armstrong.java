98e026d41e8c1f6741081448ee70d81c86c5fdfa
Prajwal2213 <prajwalm2222@gmail.com>
2025-10-07 21:47:25 +0530

    Armstrong file

---
+++ Basic_maths/Armstrong.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Armstrong{
+    public static void main(String[] args){
+        int n = 543;
+        int n1 = n;
+        int digits = String.valueOf(n).length();
+        int sum = 0;
+
+        while (n > 0){
+            int last_digit = n %10;
+            sum += Math.pow(last_digit,digits);
+            n = n/10;
+        }
+
+        if(sum == n1){
+            System.out.println("Armstrong Number");
+        }else{
+            System.out.println("Not Armstrong NUmber");
+        }
+    }
+}
