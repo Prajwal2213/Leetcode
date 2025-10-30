2102ff016e0adb0744dab07ecf09e7934b243a93
Prajwal2213 <prajwalm2222@gmail.com>
2025-10-30 15:44:36 +0530

    updated singleNumber file

--- Arrays/singleNumber_136.java
+++ Arrays/singleNumber_136.java
@@ -23,3 +23,19 @@ public class singleNumber_136{
 
     }
 }
+
+
+//optimal solution
+
+/*
+ * class Solution {
+   public int singleNumber(int[] nums) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            index=index^nums[i];
+        }
+        
+        return index;
+    }
+}
+ */
