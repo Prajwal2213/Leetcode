ef950cf376bc1ebc7709f10f435a03e6caf28ce6
Prajwal2213 <prajwalm2222@gmail.com>
2025-10-31 22:08:21 +0530

    two sneaky number problem number 3289

---
+++ Arrays/twosneakynumber3289.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] res = new int[2];
+        int j = 0;
+
+        set<Integer>
+
+        for(int i=0;i<nums.length;i++){
+           for(int k = i+1;k<nums.length;k++){
+                if(nums[i] == nums[k]){
+                    res[j] = nums[i];
+                    j++;
+                    break;
+
+                }
+           }
+        }
+    return res;
+    }
+}
