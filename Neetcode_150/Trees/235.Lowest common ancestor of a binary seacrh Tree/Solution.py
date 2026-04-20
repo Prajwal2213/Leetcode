a9517f9c839603aeaafd7453bc6e6797a4cde3ac
Prajwal2213 <prajwalm2222@gmail.com>
2026-04-20 07:21:45 +0530

    created and added lc 253 python sol

---
+++ Neetcode_150/Trees/235.Lowest common ancestor of a binary seacrh Tree/Solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+    #     def __init__(self, x):
+        #         self.val = x
+        #         self.left = None
+        #         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root :
+            return root
+
+        # If p and q values  greater than root
+        if p.val > root.val and q.val > root.val:
+            return(self.lowestCommonAncestor(root.right, p , q))
+
+        # If p and q values  less than root
+        if p.val < root.val and q.val < root.val:
+            return(self.lowestCommonAncestor(root.left, p , q))
+
+        return root
